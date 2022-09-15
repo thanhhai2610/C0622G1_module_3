90c87027499af9271a4651236ca6db71a3a9f09c
thanhhai2610 <thanhai707726@gmail.com>
2022-09-16 05:49:54 +0700

    update case study

--- module_3/src/case_study/sql_task_1_20.sql
+++ module_3/src/case_study/sql_task_1_20.sql
@@ -231,6 +231,23 @@ from  hop_dong hd
 group by hdct.ma_dich_vu_kem_theo
 having so_lan_dat <=1; 
  
+ /*
+15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, dia_chi
+ mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+*/
  
- 
+select
+ nv.ma_nhan_vien,  hd.ngay_lam_hop_dong, count(hd.ma_hop_dong) as so_lan_ki_hd,
+ nv.ho_va_ten,
+ trd.ten_trinh_do,
+ bp.ten_bo_phan,
+ nv.so_dien_thoai,
+ nv.dia_chi
+from hop_dong hd
+join nhan_vien nv on hd.ma_nhan_vien = nv.ma_nhan_vien 
+join bo_phan bp on bp.ma_bo_phan = nv.ma_bo_phan 
+join trinh_do trd on trd.ma_trinh_do = nv.ma_trinh_do 
+group by  hd.ma_nhan_vien
+having so_lan_ki_hd <3;
+
  
